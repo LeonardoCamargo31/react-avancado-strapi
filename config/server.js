34a5e083fc331e2e392e677eb4910d2cb16e4d2c
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2021-04-12 20:07:12 -0300

    feat: create project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '826fa5811beb9294c2adfaf098bd7338'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6a8d3ca-1fc2-4994-ac71-d660fe9aded8'
+};
